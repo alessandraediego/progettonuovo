09ad9496bca1f7c989653ed1990c56ae24e09bd1
alepue <alepuertas06@gmail.com>
2023-07-30 21:39:01 +0200

    aggiunto players

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("aggiunto")
+#print("aggiunto")
+import pygame, sys
+from players import Player
+pygame.init()
+
+window_size = (700, 700)
+screen = pygame.display.set_mode(window_size)
+
+pygame.display.set_caption('progetto')
+
+clock = pygame.time.Clock()
+p1 = Player(screen, (100, 100), (50, 50))
+p2 = Player(screen, (600, 600), (50, 50))
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        p1.moveup()
+    if keys[pygame.K_a]:
+        p1.moveleft()
+    if keys[pygame.K_s]:
+        p1.movedown()
+    if keys[pygame.K_d]:
+        p1.moveright()
+    if keys[pygame.K_UP]:
+        p2.moveup()
+    if keys[pygame.K_LEFT]:
+        p2.moveleft()
+    if keys[pygame.K_DOWN]:
+        p2.movedown()
+    if keys[pygame.K_RIGHT]:
+        p2.moveright()
+    screen.fill((153,17,153))
+    p1.draw()
+    p2.draw()
+    pygame.display.flip()
+
+    clock.tick(60)
---
+++ players.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Player:
+    def __init__(self, screen, pos, size) -> None:
+        self.screen = screen
+        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
+        self.image = pygame.image.load('progettonuovo/immagini/playerprova.jpg')
+        self.image = pygame.transform.scale(self.image, size)
+        self.vel = 5
+    def draw(self):
+        self.screen.blit(self.image, self.rect)
+    
+    def moveup(self):
+        self.rect.top -= self.vel
+        if self.rect.top <= 0:
+            self.rect.top = 0
+    def movedown(self):
+        
+        self.rect.bottom += self.vel
+        if self.rect.bottom >= self.screen.get_height():
+            self.rect.bottom = self.screen.get_height()
+    def moveright(self):
+        self.rect.right += self.vel
+        if self.rect.right >= self.screen.get_width():
+            self.rect.right = self.screen.get_width()
+    def moveleft(self):
+        self.rect.left -= self.vel
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        
